b540e2d4b878457e11c504779ce7924673acb91d
yeonseosla <yeonseosla@gamil.com>
2022-07-21 18:08:56 +0900

    Feat: error log data insert to db

---
+++ src/errors.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pickle
+import traceback
+from datetime import datetime
+import pandas as pd
+from access_database.access_db import AccessDataBase
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    root = sys._MEIPASS
+else:
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    root = os.path.abspath(os.path.join(cur_dir, os.pardir))
+
+tbl_cache = os.path.join(root, 'tbl_cache')
+conn_path = os.path.join(root, 'conn.txt')
+
+class Errors:
+    def __init__(self):
+        # db 연결
+        with open(conn_path, 'rb') as f:
+            conn = pickle.load(f)
+        self.db = AccessDataBase(conn[0], conn[1], conn[2])
+        
+    def errors_log(self):
+        tb = traceback.format_exc()
+        _datetime = pd.Timestamp(datetime.today())
+        table = 'error_log'
+        fields = ('traceback', 'error_date')
+        values = (tb, _datetime)
+        self.db.insert(table, fields, values)
